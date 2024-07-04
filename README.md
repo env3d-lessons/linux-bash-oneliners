a00cbe97d941a6da5fd3cf0ca732af12fca1ce6f
env3d <jmadar@gmail.com>
2024-07-04 16:30:41 -0700

    updating assignment and using a different dataset

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ and are very convenient in dealing with text based data.
 In this lab, you will be practicing using linux commands to extract and transform data
 from a local csv file and from an external web site.  
 
-You will use the data file from the BC open data website https://catalogue.data.gov.bc.ca/dataset/670516e0-6cb0-4d3b-ab2b-b82ed9ee59db/resource/8a4c39f0-0986-43eb-bc61-63e3fa4d9472/download/tui1_tuition_fees_at_public_post_secondary_institutions_by_economic_development_region.csv, which is a csv file containing
-domesic tuition for all BC post-secondary institutions.  If you are interested, you can find additional
-information for this dataset at https://open.canada.ca/data/dataset/670516e0-6cb0-4d3b-ab2b-b82ed9ee59db
+You will use the data file from the BC open data website
+
+https://catalogue.data.gov.bc.ca/dataset/6e317257-54fe-44d1-89a6-1fcd81f5a484/resource/f991757f-26f3-459a-96c4-66b3dc3f5d10/download/tui2_international_tuition_fees_at_public_post_secondary_institutions_by_economic_development_re.csv
+
+which is a csv file containing international tuition for all BC post-secondary institutions.
+If you are interested, you can find additional
+information for this dataset at https://open.canada.ca/data/en/dataset/6e317257-54fe-44d1-89a6-1fcd81f5a484
 
 # Preparation
 
@@ -31,7 +35,8 @@ text file.  For example, answer to question 1 will be put into a text file calle
 6. Find all data lines for the 2019/20 year, and show only school names and tuition
 amounts
 7. Following from question 6, sort the output in ascending order by college name
-8. (Challenging!) Following from question 6, sort the output in ascending order by tuition amount
+8. (Challenging!) Following from question 6, sort the output in ascending order by tuition amount,
+only output the insitution names
 
 # Hand-in
 
--- tests/test_assignments.py
+++ tests/test_assignments.py
@@ -1,5 +1,6 @@
 import pathlib
 import os
+import csv
 
 def file_basic_check(f):
     # Test file existence and not empty
@@ -12,24 +13,74 @@ def test_download():
 def test_q1():
     file_basic_check('q1.txt')
 
+def test_q1_content():
+    expected = os.popen('wc -l tuition.csv').read().split()[0] + '\n'
+    answer = os.popen('bash q1.txt').read()
+    assert expected == answer
+
 def test_q2():
     file_basic_check('q2.txt')
+
+def test_q2_content():
+    expected = ''.join(open('tuition.csv').readlines()[4:])
+    answer = os.popen('bash q2.txt').read()
+    assert expected == answer
 
 def test_q3():
     file_basic_check('q3.txt')
 
+def test_q3_content():
+    data = open('tuition.csv').readlines()
+    expected = ''.join(data[4]+data[-1])
+    answer = os.popen('bash q3.txt').read()
+    assert expected == answer
+    
 def test_q4():
     file_basic_check('q4.txt')
+    
+def test_q4_content():
+    expected = set([l.split(',')[3] for l in open('tuition.csv').readlines()])
+    answer = set(os.popen('bash q4.txt').read().split('\n'))
+    assert expected == answer    
     
 def test_q5():
     file_basic_check('q5.txt')
     
+def test_q5_content():
+    expected = ''.join([l for l in open('tuition.csv').readlines()
+                        if 'Capilano University' in l or 'Langara College' in l])
+    answer = os.popen('bash q5.txt').read()
+    assert expected == answer    
+    
 def test_q6():
     file_basic_check('q6.txt')
+
+def test_q6_content():
+    expected = ''.join([ ','.join(l.split(',')[3:])
+                         for l in open('tuition.csv').readlines()
+                         if '2019/20' in l])
+    answer = os.popen('bash q6.txt').read()
+    assert expected == answer    
+    
     
 def test_q7():
     file_basic_check('q7.txt')
     
+def test_q7_content():
+    expected = ''.join(sorted([ ','.join(l.split(',')[3:])
+                         for l in open('tuition.csv').readlines()
+                         if '2019/20' in l]))
+    answer = os.popen('bash q7.txt').read()
+    assert expected == answer    
+    
 def test_q8():
     file_basic_check('q8.txt')
+    
+def test_q8_content():
+    s = sorted( [ [r[3],r[4].replace('$','').replace(',','')]
+                         for r in csv.reader([l for l in open('tuition.csv').readlines() if '2019/20' in l])],
+                       key=lambda x: int(x[1]) )
+    expected = '\n'.join([r[0] for r in s]) + '\n'
+    answer = os.popen('bash q8.txt').read()
+    assert expected == answer    
 
